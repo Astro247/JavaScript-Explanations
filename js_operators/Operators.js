adcd6014aa66f6901d78a98062207704939aec5e
Astro247 <manuelsarti40@gmail.com>
2025-05-10 15:27:09 +0200

    New JavaScript Explanation: Operators and different types of operations shown.

---
+++ js_operators/Operators.js
@@ -0,0 +1,15 @@
+// Gli operatori matematici in javascript funzionano esattamente come gli operatori matematici in python.
+
+let number_one = 5, number_two = 5;
+
+console.log(`Sum of two numbers: ${number_one + number_two}`)
+
+console.log(`Difference of two numbers: ${number_one - number_two}`)
+
+console.log(`Product of two numbers: ${number_one * number_two}`)
+
+console.log(`Ratio of two numbers: ${number_one / number_two}`)
+
+console.log(`Power of two numbers: ${number_one ** number_two}`)
+
+console.log(`Module of two numbers: ${number_one % number_two}`)
