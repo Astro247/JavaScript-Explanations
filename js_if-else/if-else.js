4df6db56bac64585273e49b68af5c79a9b2bbce7
Astro247 <manuelsarti40@gmail.com>
2025-05-20 20:16:59 +0200

    Update JavaScript Explanation: If-Else + Switch + Ternary Operator explained.

--- js_if-else/if-else.js
+++ js_if-else/if-else.js
@@ -30,3 +30,27 @@ document.getElementById("submit").onclick = function() {
 let stats = 70
 approval = stats > 50 ? "hired" : "not hired"
 console.log(approval)
+
+// Un'altra alternativa ancora agli if-else sono gli switch che prendono una variabile da analizzare e la fanno passare attraverso diverse comparazioni dette 'case', con un case chiamato 'default' che esegue un codice qualora nessuno dei case abbia un match con la variabile selezionata nello switch. 
+
+let day = 4
+switch(day){ //Viene selezionato il valore della variabile "day"
+    case 1: //La domanda qui è la seguente: day == 1? uguale per tutti gli altri case
+        console.log("first day")
+        break //Il break è importante poiché possono esserci più di un match nello stesso switch.
+    case 2:
+        console.log("second day")
+        break
+    case 3:
+        console.log("third day")
+        break
+    case 4:
+        console.log("fourth day")
+        break
+    case 5:
+        console.log("five day")
+        break
+    default: //Il case 'default' viene considerato solo se tutti gli altri case non hanno alcun match.
+        console.log("no day")
+        break
+}
