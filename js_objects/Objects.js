96020c7b8e153ea0bb1af025f1e0cda105f92586
Astro247 <manuelsarti40@gmail.com>
2025-05-26 21:41:08 +0200

    Update JavaScript Explanation: Objects, keys:values, callbacks as values and constructors explained

--- js_objects/Objects.js
+++ js_objects/Objects.js
@@ -1,33 +1,34 @@
 // Gli oggetti in JavaScript equivalgono esattamente ai dizionari in Python, sono varaibili che contengono variabili chiave:valore (nome della variabile : il suo valore).
 
 
-function sayHello() {
+function functionHello() {
     return "hello world"
 }
 
 
+function car(type, color) { // Questo genere di funzioni che hanno il compito di creare oggetti si chiamano "constructors" (costruttori).
+    this.type = type // Utilizzare "this.type" equivale a scrivere "car.type".
+    this.color = color
+}
+
+
 function main() {
-    let objectPerson = {
-        name: "Astro",
-        age: 19,
-        hello: sayHello() // E' possibile richiamare come valore qualsiasi cosa, anche callbacks.
-    }
-
-    let objectLetters = {
+    let object = {
         name: "Luigi",
-        hello: function(){return `Hello ${this.name}`} // "this" fa riferimento alla key dell'oggetto su cui si è attualmente.
+        hello: functionHello(), // Utilizzare come valore una callback assegna alle key il return della funzione.
+        sayHello: function(){return `Hello ${this.name}`} // "this" fa riferimento alla key dell'oggetto su cui si è attualmente.
     }
 
-    for(let index in objectPerson) {
-        console.log(`${index} = ${objectPerson[index]}`) // Il primo index si riferisce alle key, il secondo invece si riferisce ai valori delle key ("index" è una stringa).
-    }
-
-    objectPerson.hello = "hello mondo" 
-    console.log(objectPerson["hello"]) //Inserire il nome della proprietà come stringa e come indice di un'array con array l'oggetto si riferisce al valore della key corrispondente al nome della stringa.
-    console.log(objectPerson.hello) //Anche così, inserendo il nome dell'oggetto seguito dal nome della proprietà inserita come metodo, si riferisce al valore della key corrispondente al nome del metodo.
+    object.hello = "hello mondo" 
+    console.log(object["hello"]) //Inserire il nome della proprietà come stringa e come indice di un'array con array l'oggetto si riferisce al valore della key corrispondente al nome della stringa.
+    console.log(object.hello) //Anche così, inserendo il nome dell'oggetto seguito dal nome della proprietà inserita come metodo, si riferisce al valore della key corrispondente al nome del metodo.
     
-    console.log(`Second object ${objectLetters.hello()}`) // In quanto funzione, per fare un callback è necessario aggiungere () dopo il metodo.
- 
+    console.log(object.sayHello()) // In quanto funzione, per fare un callback è necessario aggiungere () dopo il metodo.
+    
+    let firstCar = new car("Ford", "red") // La parola chiave "new" serve a generare un'oggetto partendo da una funzione.
+    let secondCar = new car("Ferrari", "yellow")
+    console.log(firstCar)
+    console.log(secondCar)
 }  
 
 main()
