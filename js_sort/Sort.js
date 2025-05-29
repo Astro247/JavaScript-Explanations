d41f39af75440f4a3c78edbbe0bcc2f8f5fcb6df
Astro247 <manuelsarti40@gmail.com>
2025-05-29 08:28:08 +0200

    New JavaScript Explanation: .sort method explained

---
+++ js_sort/Sort.js
@@ -0,0 +1,14 @@
+// In javascript il metodo .sort() ordina gli elementi di un'array in maniera alfabetica, trasformando temporaneamente tutti gli elementi dell'array in stringhe.
+
+function main() {
+    let array = [1,2,50,10,65,7]
+    console.log(array.sort()) // Gli elementi vengono trasformati in stringhe, pertanto viene letto solo il primo carattere, escludendo quelli a venire (per esempio l'elemento "10" diventa "1" e lo 0 viene rimosso, oppure l'elemento "65" diventa "6" e il 5 viene rimosso).
+    console.log(array.sort((firstN,secondN) => firstN-secondN)) // Con questa callback di una arrow function, sort ordina gli elementi numericamente in maniera crescente.
+    console.log(array.sort((firstN,secondN) => secondN-firstN)) // Così invece in maniera decrescente.
+}
+
+main()
+
+// Il funzionamento della riga di codice "array.sort((firstN,secondN) => firstN-secondN)" è molto semplice:
+// Nel momento in cui passiamo al metodo .sort una callback a una arrow funzione del genere (dove prima della freccia vengono inseriti i parametri, mentre dopo la freccia il return della funzione), il metodo .sort assegna automaticamente ai due parametri i primi due numeri dell'array (poi il secondo con il terzo, il terzo con il quarto e così via).
+// Ciò che decide quale numero viene prima e quale viene dopo è il segno della differenza nel return. Se il segno è negativo, allora il parametro prima del "-" è considerato più piccolo del secondo parametro, se invece il segno è positivo il parametro prima del "-" è considerato più grande del secondo parametro.
